dd345f6a4db4a5a9bea3d024803509a933f9da43
JornWildt <jw@fjeldgruppen.dk>
2012-01-05 13:26:32 +0100

    Started work on Json codecs.

---
+++ Ramone.Tests.Server/Codecs/CatAsJsonCodec.cs
@@ -0,0 +1,14 @@
+﻿using Ramone.Tests.Common;
+using OpenRasta.Codecs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ramone.Tests.Server.Codecs
+{
+  [MediaType("application/json")]
+  public class CatAsJsonCodec
+  {
+  }
+}
---
+++ Ramone.Tests/MediaTypes/Json/JsonSerializerCodecTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml;
+using NUnit.Framework;
+using Ramone.Tests.Common.CMS;
+using System;
+
+
+namespace Ramone.Tests.MediaTypes.Xml
+{
+  [TestFixture]
+  public class JsonSerializerCodecTests : TestHelper
+  {
+    protected override void TestFixtureSetUp()
+    {
+      base.TestFixtureSetUp();
+      //settings.CodecManager.AddCodec<HalResource>("application/hal+xml", new AtomItemCodec());
+
+    }
+    [Test]
+    public void CanReadJson()
+    {
+      // Arrange
+      RamoneRequest req = Session.Bind(DossierTemplate, new { id = 5 });
+
+      // Act
+      Dossier dossier = req.Accept("application/json").Get<Dossier>().Body;
+
+      // Assert
+      Assert.IsNotNull(dossier);
+      Assert.AreEqual(5, dossier.Id);
+    }
+
+
+    [Test]
+    public void CanWriteJson()
+    {
+      // Arrange
+
+      RamoneRequest request = Session.Request(DossiersUrl);
+
+      // Act
+      //RamoneResponse<Dossier> response = request.Post<Dossier>(dossierDoc);
+
+      // Assert
+      //Dossier createdDossier = response.Body;
+
+      //Assert.IsNotNull(createdDossier);
+      //Assert.AreEqual("My dossier", createdDossier.Title);
+    }
+  }
+}
