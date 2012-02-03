23778d1369fcf63993f194816f2fdf8bff4a3d13
JornWildt <jw@fjeldgruppen.dk>
2012-02-03 07:09:56 +0100

    Adding additional standard methods.

---
+++ Ramone.Tests/AdditionalMethodsTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+
+namespace Ramone.Tests
+{
+  [TestFixture]
+  public class AdditionalMethodsTests : TestHelper
+  {
+    [Test]
+    public void CanGetDossier()
+    {
+      // Arrange
+      RamoneRequest dossierReq = Session.Bind(DossierTemplate, new { id = 8 });
+
+      // Act
+      //RamoneResponse response = dossierReq.Head();
+
+      // Assert
+      //Assert.IsNotNull(response);
+    }
+  }
+}
