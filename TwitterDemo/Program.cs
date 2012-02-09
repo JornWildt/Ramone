8b4190468f998d6be17dc34f455a079921116d73
JornWildt <jw@fjeldgruppen.dk>
2012-02-09 22:58:10 +0100

    A working example of OAuth (simple params)

--- TwitterDemo/Program.cs
+++ TwitterDemo/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using Ramone;
+using System.Net;
+using Ramone.Utility;
 
 
 namespace TwitterDemo
@@ -15,9 +17,12 @@ namespace TwitterDemo
     static void Main(string[] args)
     {
       Setup();
-      ShowTimelineForScreenName_Dynamic(TwitterUserScreenName);
-      Console.WriteLine();
-      ShowTimelineForScreenName_Static(TwitterUserScreenName);
+
+      //ShowTimelineForScreenName_Dynamic(TwitterUserScreenName);
+      //Console.WriteLine();
+      //ShowTimelineForScreenName_Static(TwitterUserScreenName);
+
+      UpdateUserName();
     }
 
 
@@ -28,8 +33,10 @@ namespace TwitterDemo
 
       // Set default request/response media-type to JSON for Twitter.
       // This saves us the hassle of specifying codecs for all the Twitter resource types (Tweet, Timeline, User etc.)
-      Session.DefaultRequestMediaType = "application/json";
+      Session.DefaultRequestMediaType = "application/x-www-form-urlencoded";
       Session.DefaultResponseMediaType = "application/json";
+      
+      Session.RequestInterceptors.Add(new OAuthInterceptor());
     }
 
 
@@ -73,6 +80,83 @@ namespace TwitterDemo
     }
 
 
+    static void UpdateUserName()
+    {
+      RamoneRequest request = Session.Bind(TwitterApi.UpdateProfileTemplate, new { name = "Peter-Pedal-2" });
+      RamoneResponse response = request.Post(new { });
+    }
+
+
+    static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+
+    static double GetUnixTime()
+    {
+      TimeSpan span = (DateTime.Now.ToLocalTime() - epoch);
+      return span.TotalSeconds;
+    }
+
+
+    public class OAuthInterceptor : IRequestInterceptor
+    {
+      #region IRequestInterceptor Members
+
+      public void Intercept(HttpWebRequest request)
+      {
+        OAuthBase o = new OAuthBase();
+
+        string consumer_key = "FrxT7YcvlC5l8H4cNvxp1A";
+        string consumer_secret = "rL7ddb1g4OegHTLAInQAZGrF92RW72j6kQ9e1OXR6xI";
+
+        string access_token = "348919657-4q4XSvhsuZuasUVTNpdmDWjCZzmDsP0yTMV819zh";
+        string access_token_secret = "PAoxLvWhHVXZeOOUAHklKopZzblLZDXaHxv3DQd7I";
+
+        string timestamp = o.GenerateTimeStamp();
+        string nonce = o.GenerateNonce();
+
+        timestamp = "1328824349";
+        nonce = "c54be2858d15ba0e61f27353729e13d6";
+
+        string url;
+        string requestParams;
+
+        string signature = o.GenerateSignature(request.RequestUri,
+                                               consumer_key,
+                                               consumer_secret,
+                                               access_token,
+                                               access_token_secret,
+                                               request.Method,
+                                               timestamp,
+                                               nonce,
+                                               OAuthBase.SignatureTypes.HMACSHA1,
+                                               out url,
+                                               out requestParams);
+
+        //string signatureBaseString = o.GenerateSignatureBase(request.RequestUri,
+        //                                                     consumer_key,
+        //                                                     access_token,
+        //                                                     access_token_secret,
+        //                                                     request.Method.ToUpper(),
+        //                                                     timestamp,
+        //                                                     nonce,
+        //                                                     "HMAC-SHA1",
+        //                                                     out url,
+        //                                                     out requestParams);
+
+        string auth = string.Format(@"OAuth oauth_consumer_key=""{1}"", oauth_nonce=""{3}"", oauth_signature=""{6}"", oauth_signature_method=""{5}"", oauth_timestamp=""{4}"", oauth_token=""{2}"", oauth_version=""1.0""",
+
+        url,
+        consumer_key,
+        access_token,
+        nonce,
+        timestamp,
+        "HMAC-SHA1",
+        o.UrlEncode(signature));
+
+        request.Headers["Authorization"] = auth;
+      }
+
+      #endregion
+    }
 
 #if false
     // Ignore this for now ...
--- TwitterDemo/TwitterApi.cs
+++ TwitterDemo/TwitterApi.cs
@@ -8,7 +8,9 @@ namespace TwitterDemo
   public class TwitterApi
   {
     public const string UserTimeLinePath = "statuses/user_timeline.json?screen_name={screen_name}&count={count}";
+    public const string UpdateProfilePath = "account/update_profile.json?name={name}";
 
     public static UriTemplate UserTimeLineTemplate = new UriTemplate(UserTimeLinePath);
+    public static UriTemplate UpdateProfileTemplate = new UriTemplate(UpdateProfilePath);
   }
 }
