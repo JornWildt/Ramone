e4ec4612baf402ca804f28223b819dd7b258916a
JornWildt <jw@fjeldgruppen.dk>
2012-02-19 20:58:34 +0100

    Proving that exception body can be handled by codecs.

---
+++ Ramone.Tests/ExceptionHandlingTests.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using NUnit.Framework;
+
+
+namespace Ramone.Tests
+{
+  [TestFixture]
+  public class ExceptionHandlingTests : TestHelper
+  {
+    [Test]
+    public void WhenCatchingAuthorizationExceptionItAllowsToDeserializeContent()
+    {
+      try
+      {
+        Session.Request(BasicAuthUrl).Get<string>();
+        Assert.Fail("Missing exception.");
+      }
+      catch (NotAuthorizedException ex)
+      {
+        HtmlDocument error = ex.Response.AsRamoneResponse<HtmlDocument>(Session).Body;
+        Assert.IsNotNull(error);
+      }
+    }
+  }
+}
