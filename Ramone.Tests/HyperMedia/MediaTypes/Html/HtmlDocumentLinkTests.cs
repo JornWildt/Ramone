bed06c4dbeee37ffde0f23c946c5609e3b6cf6e1
JornWildt <jw@fjeldgruppen.dk>
2012-02-21 22:20:36 +0100

    Working with HTML links.

---
+++ Ramone.Tests/HyperMedia/MediaTypes/Html/HtmlDocumentLinkTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using HtmlAgilityPack;
+using Ramone.HyperMedia;
+using Ramone.HyperMedia.Html;
+
+
+namespace Ramone.Tests.HyperMedia.MediaTypes.Html
+{
+  [TestFixture]
+  public class HtmlDocumentLinkTests : TestHelper
+  {
+    HtmlDocument HtmlDoc;
+
+    string Html = @"
+<html>
+  <body>
+    <p>Hello World: <a href=""http://link1"" rel=""next"">Link no. 1</a></p>
+    <a>A-Bomb!</a>
+    <div id=""set1"">
+      <p><a href=""http://link2"" rel=""up"">Link no. 2</a></p>
+      <a>B-Bomb!</a>
+    </div>
+  </body>
+</html>";
+
+
+    protected override void TestFixtureSetUp()
+    {
+      base.TestFixtureSetUp();
+      HtmlDoc = new HtmlDocument();
+      HtmlDoc.LoadHtml(Html);
+    }
+
+
+    [Test]
+    public void CanExtractLinksFromHtmlDocument()
+    {
+      // Act
+      List<ILink> links = HtmlDoc.Links().ToList();
+
+      // Assert
+      Assert.AreEqual(4, links.Count);
+
+      ILink l1 = links[0];
+      Assert.AreEqual("http://link1", l1.HRef);
+      Assert.AreEqual("Link no. 1", l1.Title);
+      Assert.AreEqual("next", l1.RelationshipType);
+
+      ILink l2 = links[2];
+      Assert.AreEqual("http://link2", l2.HRef);
+      Assert.AreEqual("Link no. 2", l2.Title);
+      Assert.AreEqual("up", l2.RelationshipType);
+    }
+
+
+    [Test]
+    public void CanExtractLinksFromHtmlNode()
+    {
+      // Arrange (get sub-node)
+      HtmlNode node = HtmlDoc.DocumentNode.SelectNodes(@"//div[@id=""set1""]").First();
+
+      // Act
+      List<ILink> links = node.Links().ToList();
+
+      // Assert
+      Assert.AreEqual(2, links.Count);
+      ILink l1 = links[0];
+
+      Assert.AreEqual("http://link2", l1.HRef);
+      Assert.AreEqual("Link no. 2", l1.Title);
+      Assert.AreEqual("up", l1.RelationshipType);
+    }
+
+
+    [Test]
+    public void CanExtractLinkByRelationFromHtmlDocument()
+    {
+      // Act
+      ILink link1 = HtmlDoc.Link("next");
+      ILink link2 = HtmlDoc.Link("up");
+
+      // Assert
+      Assert.AreEqual("http://link1", link1.HRef);
+      Assert.AreEqual("Link no. 1", link1.Title);
+      Assert.AreEqual("next", link1.RelationshipType);
+    
+      Assert.AreEqual("http://link2", link2.HRef);
+      Assert.AreEqual("Link no. 2", link2.Title);
+      Assert.AreEqual("up", link2.RelationshipType);
+    }
+
+
+    [Test]
+    public void CanExtractLinkByRelationFromHtmlNode()
+    {
+      // Arrange (get sub-node)
+      HtmlNode node = HtmlDoc.DocumentNode.SelectNodes(@"//div[@id=""set1""]").First();
+
+      // Act
+      ILink link1 = node.Link("next");
+      ILink link2 = node.Link("up");
+
+      // Assert
+      Assert.IsNull(link1);
+
+      Assert.AreEqual("http://link2", link2.HRef);
+      Assert.AreEqual("Link no. 2", link2.Title);
+      Assert.AreEqual("up", link2.RelationshipType);
+    }
+  }
+}
---
+++ Ramone/HyperMedia/Html/HtmlExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CuttingEdge.Conditions;
+using HtmlAgilityPack;
+using Ramone.HyperMedia.Atom;
+
+
+namespace Ramone.HyperMedia.Html
+{
+  public static class HtmlExtensions
+  {
+    public static IEnumerable<ILink> Links(this HtmlDocument html)
+    {
+      Condition.Requires(html, "html").IsNotNull();
+
+      return html.DocumentNode
+                 .SelectNodes("//a")
+                 .Select(a => new AtomLink(GetAttribute(a, "href"), GetAttribute(a, "rel"), null, a.InnerText));
+    }
+
+
+    public static IEnumerable<ILink> Links(this HtmlNode node)
+    {
+      Condition.Requires(node, "node").IsNotNull();
+
+      return node.SelectNodes(".//a")
+                 .Select(a => new AtomLink(GetAttribute(a, "href"), GetAttribute(a, "rel"), null, a.InnerText));
+    }
+
+
+    public static ILink Link(this HtmlDocument html, string rel)
+    {
+      Condition.Requires(html, "html").IsNotNull();
+      Condition.Requires(rel, "rel").IsNotNull();
+
+      return html.DocumentNode
+                 .SelectNodes("//a")
+                 .Where(a => GetAttribute(a, "rel") == rel)
+                 .Select(a => new AtomLink(GetAttribute(a, "href"), GetAttribute(a, "rel"), null, a.InnerText))
+                 .FirstOrDefault();
+    }
+
+
+    public static ILink Link(this HtmlNode node, string rel)
+    {
+      Condition.Requires(node, "node").IsNotNull();
+      Condition.Requires(rel, "rel").IsNotNullOrEmpty();
+
+      return node.SelectNodes(".//a")
+                 .Where(a => GetAttribute(a,"rel") == rel)
+                 .Select(a => new AtomLink(GetAttribute(a,"href"), GetAttribute(a,"rel"), null, a.InnerText))
+                 .FirstOrDefault();
+    }
+
+
+    private static string GetAttribute(HtmlNode node, string name)
+    {
+      if (node == null)
+        return null;
+      
+      HtmlAttribute a = node.Attributes[name];
+      if (a == null)
+        return null;
+
+      return a.Value;
+    }
+  }
+}
