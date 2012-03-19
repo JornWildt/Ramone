2b6ddf2b089acecd4d41bae14fb15e9a021d3925
JornWildt <jw@fjeldgruppen.dk>
2012-03-19 12:47:07 +0100

    Testing Atom link format.

--- Ramone.Tests/MediaTypes/Atom/AtomTests.cs
+++ Ramone.Tests/MediaTypes/Atom/AtomTests.cs
@@ -3,6 +3,9 @@ using System.ServiceModel.Syndication;
 using NUnit.Framework;
 using Ramone.MediaTypes.Atom;
 using Ramone.HyperMedia;
+using System.Xml.Serialization;
+using System.IO;
+using System;
 
 
 namespace Ramone.Tests.MediaTypes.Atom
@@ -56,8 +59,34 @@ namespace Ramone.Tests.MediaTypes.Atom
     }
 
 
-    class MyResource
+    [Test]
+    public void CanSerializeAtomLinksToCorrectFormat()
     {
+      // Arrange
+      string correctXml = @"<?xml version=""1.0"" encoding=""utf-16""?>
+<MyResource xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+  <link href=""http://dr.dk"" rel=""test"" type=""text/html"" title=""DR"" xmlns=""http://www.w3.org/2005/Atom"" />
+</MyResource>";
+
+      XmlSerializer serializer = new XmlSerializer(typeof(MyResource));
+      MyResource r = new MyResource();
+      r.Links.Add(new AtomLink("http://dr.dk", "test", "text/html", "DR"));
+
+      // Act
+      using (StringWriter w = new StringWriter())
+      {
+        serializer.Serialize(w, r);
+
+        string xml = w.ToString();
+        Console.Write(xml);
+        Assert.AreEqual(correctXml, xml);
+      }
+    }
+
+
+    public class MyResource
+    {
+      [XmlElement(ElementName="link", Namespace=AtomConstants.AtomNamespace)]
       public AtomLinkList Links { get; set; }
 
       public MyResource()
