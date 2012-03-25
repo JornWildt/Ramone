c80843f74200be682b0f1f0bf2e777dfecbb1ddc
JornWildt <jw@fjeldgruppen.dk>
2012-03-25 10:34:20 +0200

    Test null header.

--- Ramone.Tests/HyperMedia/LinkHeaderTests.cs
+++ Ramone.Tests/HyperMedia/LinkHeaderTests.cs
@@ -24,5 +24,21 @@ namespace Ramone.Tests.HyperMedia
       Assert.AreEqual("http://example.com/TheBook/chapter2", links[0].HRef.AbsoluteUri);
       Assert.AreEqual("http://example.com/TheBook/chapter4", links[1].HRef.AbsoluteUri);
     }
+
+
+    [Test]
+    public void WhenLinkHeaderIsEmptyOrNullItRetursEmptyList()
+    {
+      // Arrange
+      Request request = Session.Bind(CatTemplate, new { name = "Alex" });
+
+      // Act
+      Response response = request.Get();
+
+      // Assert
+      List<WebLink> links = response.Links().ToList();
+      Assert.IsNotNull(links);
+      Assert.AreEqual(0, links.Count);
+    }
   }
 }
