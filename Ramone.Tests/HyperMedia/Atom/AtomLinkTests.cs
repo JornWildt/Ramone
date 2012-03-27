fad870e63eca0ea36e88db9684aa7a50dbda20d2
JornWildt <jw@fjeldgruppen.dk>
2012-03-27 07:07:24 +0200

    Atom links.

---
+++ Ramone.Tests/HyperMedia/Atom/AtomLinkTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.ServiceModel.Syndication;
+using Ramone.MediaTypes.Atom;
+using Ramone.HyperMedia;
+
+namespace Ramone.Tests.HyperMedia.Atom
+{
+  [TestFixture]
+  public class AtomLinkTests : TestHelper
+  {
+    SyndicationFeed Feed;
+
+
+    protected override void TestFixtureSetUp()
+    {
+      base.TestFixtureSetUp();
+      SyndicationItem item1 = new SyndicationItem("Item 1", "Blah", new Uri("http://link1"));
+      SyndicationItem item2 = new SyndicationItem("Item 2", "Blah", new Uri("http://link2"));
+      List<SyndicationItem> items = new List<SyndicationItem> { item1, item2 };
+      Feed = new SyndicationFeed("My Feed", "Blah", new Uri("http://feed"), items);
+      Feed.Links.Add(new SyndicationLink(new Uri("http://edit"), "edit", "Edit feed", "text/html", 0));
+    }
+
+
+    [Test]
+    public void CanGetLinksFromFeed()
+    {
+      // Act
+      List<AtomLink> links = Feed.Links(BaseUrl).ToList();
+
+      // Assert
+      Assert.AreEqual(2, links.Count);
+
+      ILink l1 = links[0];
+      Assert.AreEqual("http://feed/", l1.HRef.AbsoluteUri);
+      Assert.IsNull(l1.Title);
+
+      ILink l2 = links[1];
+      Assert.AreEqual("http://edit/", l2.HRef.AbsoluteUri);
+      Assert.AreEqual("Edit feed", l2.Title);
+      Assert.Contains("edit", l2.RelationTypes.ToList());
+    }
+
+
+    [Test]
+    public void CanGetLinksFromItem()
+    {
+      // Act
+      List<AtomLink> links = Feed.Items.First().Links(BaseUrl).ToList();
+
+      // Assert
+      Assert.AreEqual(1, links.Count);
+
+      ILink l1 = links[0];
+      Assert.AreEqual("http://link1/", l1.HRef.AbsoluteUri);
+      Assert.IsNull(l1.Title);
+    }
+  }
+}
---
+++ Ramone/MediaTypes/Atom/AtomLinkExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+
+
+namespace Ramone.MediaTypes.Atom
+{
+  public static class AtomLinkExtensions
+  {
+    /// <summary>
+    /// Get all links in the syndication feed as a sequence of ILink. Do not include links from feed items.
+    /// </summary>
+    public static IEnumerable<AtomLink> Links(this SyndicationFeed feed, Response response)
+    {
+      return Links(feed, response != null ? response.BaseUri : null);
+    }
+
+
+    public static IEnumerable<AtomLink> Links(this SyndicationFeed feed, Uri baseUrl)
+    {
+      if (feed == null)
+        return Enumerable.Empty<AtomLink>();
+
+      return feed.Links.Select(l => new AtomLink(l.Uri, l.RelationshipType, l.MediaType, l.Title));
+    }
+
+
+    /// <summary>
+    /// Get all links in the syndication item as a sequence of ILink.
+    /// </summary>
+    public static IEnumerable<AtomLink> Links(this SyndicationItem item, Response response)
+    {
+      return Links(item, response != null ? response.BaseUri : null);
+    }
+
+
+    public static IEnumerable<AtomLink> Links(this SyndicationItem item, Uri baseUrl)
+    {
+      if (item == null)
+        return Enumerable.Empty<AtomLink>();
+
+      return item.Links.Select(l => new AtomLink(l.Uri, l.RelationshipType, l.MediaType, l.Title));
+    }
+  }
+}
