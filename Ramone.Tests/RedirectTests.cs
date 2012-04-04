3a5e7dc6f9e90489d176647c0b015a3652056b7b
JornWildt <jw@fjeldgruppen.dk>
2012-04-04 14:05:25 +0200

    Starting work on redirects.

---
+++ Ramone.Tests/RedirectTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+
+namespace Ramone.Tests
+{
+  [TestFixture]
+  public class RedirectTests : TestHelper
+  {
+    [Test]
+    public void ByDefaultItFollowsRedirects()
+    {
+      Assert.Fail();
+    }
+
+
+    [Test]
+    public void RedirectCountCanBeSpecified()
+    {
+      Assert.Fail();
+    }
+
+
+    [Test]
+    public void WithRedirectCountSetToZeroItDoesNotFollowRedirects()
+    {
+      Assert.Fail();
+    }
+
+
+    [Test]
+    public void WhenFollowingRedirectsItAppliesRequestInterceptors()
+    {
+      Assert.Fail();
+    }
+  }
+}
