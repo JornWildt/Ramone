47f58b588f9e4338c4c722cb14a2bf89a80ab807
JornWildt <jw@fjeldgruppen.dk>
2012-05-16 15:27:33 +0200

    Renaming

--- post-build.bat
+++ post-build.bat
@@ -3,7 +3,6 @@ copy Ramone.dll Ramone.Core.dll
 "C:\Program Files\Microsoft\ILMerge\ilmerge.exe" /target:library /out:Ramone.dll Ramone.Core.dll JsonFx.dll HtmlAgilityPack.dll CuttingEdge.Conditions.dll /targetplatform:v4
 xcopy Ramone*.dll ..\..\..\Binaries\ /S /I /R /Y
 xcopy Ramone*.pdb ..\..\..\Binaries\ /S /I /R /Y
-xcopy Ramone*.xml ..\..\..\Binaries\ /S /I /R /Y
+rem xcopy Ramone*.xml ..\..\..\Binaries\ /S /I /R /Y
 
 cd ..\..\..
-
