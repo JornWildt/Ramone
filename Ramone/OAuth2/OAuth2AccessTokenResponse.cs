49d8e4fa16d814a6d094970a4ead2e2bbda31bc7
JornWildt <jw@fjeldgruppen.dk>
2013-01-17 22:38:02 +0100

    Doc fixes.

--- Ramone/OAuth2/OAuth2AccessTokenResponse.cs
+++ Ramone/OAuth2/OAuth2AccessTokenResponse.cs
@@ -1,5 +1,9 @@
 ﻿namespace Ramone.OAuth2
 {
+  /// <summary>
+  /// A class representing the response from an access token request.
+  /// </summary>
+  /// <remarks>See http://tools.ietf.org/html/rfc6749#section-4.1.4</remarks>
   public class OAuth2AccessTokenResponse
   {
     public string access_token { get; set; }
--- Ramone/OAuth2/OAuth2Settings.cs
+++ Ramone/OAuth2/OAuth2Settings.cs
@@ -9,31 +9,31 @@ namespace Ramone.OAuth2
   public class OAuth2Settings
   {
     /// <summary>
-    /// Authorization endpoint URL as specified in RFC 6749.
+    /// Server authorization endpoint URL.
     /// </summary>
     /// <remarks>See http://tools.ietf.org/html/rfc6749#section-3.1</remarks>
     public Uri AuthorizationEndpoint { get; set; }
 
     /// <summary>
-    /// Token endpoint URL as specified in RFC 6749.
+    /// Server token endpoint URL.
     /// </summary>
     /// <remarks>See http://tools.ietf.org/html/rfc6749#section-3.2</remarks>
     public Uri TokenEndpoint { get; set; }
 
     /// <summary>
-    /// Client redirection endpoint URL as specified in RFC 6749.
+    /// Client redirection endpoint URL.
     /// </summary>
     /// <remarks>See http://tools.ietf.org/html/rfc6749#section-3.1.2</remarks>
     public Uri RedirectUri { get; set; }
 
     /// <summary>
-    /// Client identifier as specified in RFC 6749.
+    /// Client identifier.
     /// </summary>
     /// <remarks>See http://tools.ietf.org/html/rfc6749#section-2.2</remarks>
     public string ClientID { get; set; }
 
     /// <summary>
-    /// Client secret (password) as specified in RFC 6749.
+    /// Client secret (password).
     /// </summary>
     /// <remarks>See http://tools.ietf.org/html/rfc6749#section-2.3.1</remarks>
     public string ClientSecret { get; set; }
