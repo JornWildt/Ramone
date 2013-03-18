05f1772c3dc0f845d3080f44cfc17b5322c30be9
JornWildt <jw@fjeldgruppen.dk>
2013-03-18 17:16:30 +0100

    Make sure post build script works on both 32 and 64 bit platforms.

--- post-build.bat
+++ post-build.bat
@@ -1,14 +1,14 @@
+SET ProgFiles86Root=%ProgramFiles(x86)%
+IF NOT "%ProgFiles86Root%"=="" GOTO win64
+SET ProgFiles86Root=%ProgramFiles%
+:win64
+
 cd Ramone\bin\debug
 copy Ramone.dll Ramone.Core.dll 
 
-rem "C:\Program Files\Microsoft\ILMerge\ilmerge.exe" /target:library /out:Ramone.dll Ramone.Core.dll JsonFx.dll HtmlAgilityPack.dll CuttingEdge.Conditions.dll /targetplatform:v4
-
-"C:\Program Files (x86)\Microsoft\ILMerge\ilmerge.exe" /target:library /out:Ramone.dll Ramone.Core.dll JsonFx.dll HtmlAgilityPack.dll CuttingEdge.Conditions.dll /targetplatform:v4
+"%ProgFiles86Root%\Microsoft\ILMerge\ilmerge.exe" /target:library /out:Ramone.dll Ramone.Core.dll JsonFx.dll HtmlAgilityPack.dll CuttingEdge.Conditions.dll /targetplatform:v4
 
 xcopy Ramone*.dll ..\..\..\Binaries\ /S /I /R /Y
 xcopy Ramone*.pdb ..\..\..\Binaries\ /S /I /R /Y
-rem xcopy Ramone*.xml ..\..\..\Binaries\ /S /I /R /Y
 
 cd ..\..\..
-
-copy Documentation\Source\Ramone.pdf Documentation\Ramone.pdf 
