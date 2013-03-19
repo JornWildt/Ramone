c5010f192460a6d23256441ed2673533c7d19576
JornWildt <jw@fjeldgruppen.dk>
2013-03-19 07:00:41 +0100

    Better build scripts

---
+++ make-doc.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\LibreOffice 3.6\program\soffice.exe" --headless --convert-to pdf Documentation\Ramone.odt --outdir Documentation
--- post-build.bat
+++ post-build.bat
@@ -1,14 +1,2 @@
-SET ProgFiles86Root=%ProgramFiles(x86)%
-IF NOT "%ProgFiles86Root%"=="" GOTO win64
-SET ProgFiles86Root=%ProgramFiles%
-:win64
-
-cd Ramone\bin\debug
-copy Ramone.dll Ramone.Core.dll 
-
-"%ProgFiles86Root%\Microsoft\ILMerge\ilmerge.exe" /target:library /out:Ramone.dll Ramone.Core.dll JsonFx.dll HtmlAgilityPack.dll CuttingEdge.Conditions.dll /targetplatform:v4
-
-xcopy Ramone*.dll ..\..\..\Binaries\ /S /I /R /Y
-xcopy Ramone*.pdb ..\..\..\Binaries\ /S /I /R /Y
-
-cd ..\..\..
+call make-bin.bat
+call make-doc.bat
