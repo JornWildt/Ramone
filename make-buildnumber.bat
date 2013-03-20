e021367ad4858fe6d0f443afa920cf43a63b6b9d
JornWildt <jw@fjeldgruppen.dk>
2013-03-20 22:17:08 +0100

    Add version number script.

---
+++ make-buildnumber.bat
@@ -0,0 +1,4 @@
+c:\bin\sed --text "s/AssemblyFileVersion(\"\([0-9]*\)\.\([0-9]*\)\.\(.*\)\")\]/AssemblyFileVersion(\"\1.\2.0.%1\")]/g" Ramone\Properties\AssemblyInfo.cs > Ramone\Properties\AssemblyInfo-2.cs
+
+del Ramone\Properties\AssemblyInfo.cs
+ren Ramone\Properties\AssemblyInfo-2.cs AssemblyInfo.cs
