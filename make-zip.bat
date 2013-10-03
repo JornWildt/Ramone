503e8d3ecadf4031415bceccfaa80c2fb47dc9bd
JornWildt <jw@fjeldgruppen.dk>
2013-10-03 22:23:38 +0200

    Include release notes in zip.

--- make-zip.bat
+++ make-zip.bat
@@ -1,2 +1,2 @@
 del Ramone.zip
-"C:\Program files\7-Zip\7z.exe" a Ramone.zip README.TXT LICENSE.TXT Binaries Documentation\*.pdf
+"C:\Program files\7-Zip\7z.exe" a Ramone.zip README.TXT LICENSE.TXT RELEASE-NOTES.TXT Binaries Documentation\*.pdf
