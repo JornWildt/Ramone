3b312edf7403eab537cffbda0e64a5604152109a
JornWildt <jw@fjeldgruppen.dk>
2014-01-25 21:34:13 +0100

    Add memory file.

---
+++ Ramone/IO/MemoryFile.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+
+namespace Ramone.IO
+{
+  public class MemoryFile : Ramone.IO.IFile
+  {
+    #region IFile Members
+
+    public string ContentType { get; set; }
+
+    public string Filename { get; set; }
+
+    public Stream OpenStream()
+    {
+      return new MemoryStream(Bytes);
+    }
+
+    #endregion
+
+    public byte[] Bytes { get; set; }
+  }
+}
