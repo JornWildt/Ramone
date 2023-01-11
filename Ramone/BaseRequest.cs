JornWildt/Ramone

commit ac011961d93940de79e0b7be4987759498942ae2

Use System.Net.Http.Headers.AuthenticationHeaderValue to parse authentication header

Ramone/BaseRequest.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.IO;
 using System.Net;
+using System.Net.Http.Headers;
 
 namespace Ramone
 {
@@ -408,20 +409,18 @@ namespace Ramone
     }
 
 
-    private bool HandleUnauthorized(HttpWebResponse response, WebException ex)
+    private bool HandleUnauthorized(HttpWebResponse response)
     {
       string authenticationHeader = response.Headers["WWW-Authenticate"];
-      if (!string.IsNullOrEmpty(authenticationHeader))
-      {
-        int pos = authenticationHeader.IndexOf(' ');
-        string scheme = authenticationHeader.Substring(0, pos);
-        string parameters = authenticationHeader.Substring(pos + 1);
-        IAuthorizationHandler handler = Session.AuthorizationDispatcher.Get(scheme);
-        if (handler != null && handler.HandleAuthorizationRequest(new AuthorizationContext(Session, response, scheme, parameters)))
-          return true;
-      }
 
-      return false;
+      if (!AuthenticationHeaderValue.TryParse(authenticationHeader, out AuthenticationHeaderValue authenticationHeaderValue))
+        return false;
+
+      IAuthorizationHandler handler = Session.AuthorizationDispatcher.Get(authenticationHeaderValue.Scheme);
+
+      return handler != null
+          && handler.HandleAuthorizationRequest(
+               new AuthorizationContext(Session, response, authenticationHeaderValue.Scheme, authenticationHeaderValue.Parameter));
     }
   }
 }

Ramone.Tests/AuthenticationHeaderParsingTests.cs
@@ -1,0 +1,63 @@
+using System;
+using System.Net.Http.Headers;
+using NUnit.Framework;
+
+
+namespace Ramone.Tests
+{
+  [TestFixture]
+  public class AuthenticationHeaderValueTests
+  {
+    private void CanParseBase(string authenticationHeader, string expectedScheme, string expectedParameters)
+    {
+      // Act
+      AuthenticationHeaderValue authenticationHeaderValue = AuthenticationHeaderValue.Parse(authenticationHeader);
+
+      // Assert
+      Assert.That(authenticationHeaderValue.Scheme, Is.EqualTo(expectedScheme));
+      Assert.That(authenticationHeaderValue.Parameter, Is.EqualTo(expectedParameters));
+    }
+
+
+    [Test]
+    public void CanParseWithNoParameters() => CanParseBase(
+      authenticationHeader: "Basic",
+      expectedScheme: "Basic",
+      expectedParameters: null);
+
+
+    [Test]
+    public void CanParseSchemeWithTrailingWhitespace() => CanParseBase(
+      authenticationHeader: "Basic ",
+      expectedScheme: "Basic",
+      expectedParameters: null);
+
+
+    [Test]
+    public void CanParseSchemeWithTrailingWhitespaces() => CanParseBase(
+      authenticationHeader: "Basic  ",
+      expectedScheme: "Basic",
+      expectedParameters: null);
+
+
+    [Test]
+    public void CanParseSchemeAndParameter() => CanParseBase(
+      authenticationHeader: "Basic dGVzdDoxMjPCow==",
+      expectedScheme: "Basic",
+      expectedParameters: "dGVzdDoxMjPCow==");
+
+
+    [Test]
+    public void CanParseSchemeAndParameters() => CanParseBase(
+      authenticationHeader: "Basic dGVzdDoxMjPCow==, charset=\"UTF-8\"",
+      expectedScheme: "Basic",
+      expectedParameters: "dGVzdDoxMjPCow==, charset=\"UTF-8\"");
+
+
+    [TestCase(null)]
+    [TestCase("")]
+    [TestCase(" ")]
+    public void CannotParse(string authenticationHeader) =>
+      Assert.That(() => AuthenticationHeaderValue.Parse(authenticationHeader), Throws.InstanceOf<FormatException>());
+  }
+}

Ramone.Tests/Ramone.Tests.csproj
@@ -63,6 +63,7 @@
     <Reference Include="System.Core">
       <RequiredTargetFramework>3.5</RequiredTargetFramework>
     </Reference>
+    <Reference Include="System.Net.Http" />
     <Reference Include="System.ServiceModel" />
     <Reference Include="System.ServiceModel.Web">
       <RequiredTargetFramework>3.5</RequiredTargetFramework>
@@ -84,6 +85,7 @@
     <Compile Include="ApplicationErrorTests.cs" />
     <Compile Include="AsyncEventTests.cs" />
     <Compile Include="AsyncTests.cs" />
+    <Compile Include="AuthenticationHeaderParsingTests.cs" />
     <Compile Include="BindingTests.cs" />
     <Compile Include="Blog\BlogTestHelper.cs" />
     <Compile Include="Blog\Codecs\Html\AuthorCodec_Html.cs" />

Ramone/Ramone.csproj
@@ -6,6 +6,7 @@
 
   <ItemGroup Condition=" '$(TargetFramework)' == 'net48' ">
     <Reference Include="System.Web" />
+    <Reference Include="System.Net.Http" />
   </ItemGroup>
   
   <PropertyGroup>

commit 910d5943cb3503461b9806985b88fde0151eba39

Fix build
Fix build broken by https://github.com/cBrain-dk/Ramone/commit/ac011961d93940de79e0b7be4987759498942ae2

Ramone/BaseRequest.cs
@@ -1,4 +1,4 @@
-﻿using Ramone.Utility.Validation;
+using Ramone.Utility.Validation;
 using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
@@ -388,7 +388,7 @@ namespace Ramone
       {
         if (response.StatusCode == HttpStatusCode.Unauthorized)
         {
-          if (!HandleUnauthorized(response, ex))
+          if (!HandleUnauthorized(response))
             return new HandleWebExceptionResult();
 
           if (retryLevel == 0)